0bdc13b3ed46cfdea67282f6d90cbd3dfc92b029
Tiago Ribeiro <tiago.err@outlook.com>
2021-11-10 19:33:37 +0000

    Added most of the endpoints that should be needed

--- routes/api.ts
+++ routes/api.ts
@@ -13,4 +13,30 @@ export const api = (app: Express) => {
 
 	app.get("/banks");
 	app.get("/bank/:id");
+	app.post("/banks");
+	app.put("/banks");
+
+	app.get("/subscriptions");
+	app.get("/subscription/:id");
+	app.post("/subscriptions");
+	app.put("/subscriptions");
+
+	app.get("/transactions");
+	app.get("/bank/:id/transactions");
+	app.get("/transaction/:id");
+	app.post("/transactions");
+	app.put("/transactions");
+
+	app.get("/categories");
+	app.get("/category/:id");
+	app.post("/categories");
+	app.put("/categories");
+
+	app.get("/monthy-balance");
+	app.get("/yearly-balance");
+
+	app.get("/user");
+	app.post("/users");
+	app.put("/users");
+	app.post("/login");
 };
